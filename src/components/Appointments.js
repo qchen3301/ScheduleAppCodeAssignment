b7c6675b1bc7a8d133e619773d84a2f2e852af64
qchen3301 <q.chen.3301@gmail.com>
2019-01-23 15:56:00 -0500

    ternary to toggle modal visiblity working

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import UserModal from './UserModal'
 
-
 export default class Appointments extends Component {
     state = {
         modalVisible: false,
@@ -20,11 +19,11 @@ export default class Appointments extends Component {
             btnColor: "red",
             modalVisible: !this.state.modalVisible
         })
+
         console.log("You clicked 9AM " + this.state.clicks + " amount of times")
         console.log("The button color is now " + this.state.btnColor)
         console.log("Modal open? " + this.state.modalVisible)
     }
-
 
   render() {
 
@@ -32,17 +31,10 @@ export default class Appointments extends Component {
       <div>
         Hello World from Appointments Component! <br />
         Select Your Appointment Time Below <br />
-
-        <UserModal show={this.state.modalVisible}/>
-        <button style={{background: this.state.btnColor}} onClick={this.alertTest}>9 AM</button>
-        <button>10 AM</button>
-        <button>11 AM</button>
-        <button>12 PM Noon</button>
-        <button>1 PM</button>
-        <button>2 PM</button>
-        <button>3 PM</button>
-        <button>4 PM</button>
-        <button>5 PM</button> 
+        <button 
+            style={{background: this.state.btnColor}} 
+            onClick={this.alertTest}>9 AM</button>
+        {this.state.modalVisible ? <UserModal/>:null}
       </div>
     )
   }
