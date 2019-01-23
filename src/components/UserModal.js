031f96fe26d66b007af19df8b8353d40a42ee323
qchen3301 <q.chen.3301@gmail.com>
2019-01-23 11:59:39 -0500

    added styled-components package

--- src/components/UserModal.js
+++ src/components/UserModal.js
@@ -6,7 +6,10 @@ export default class UserModal extends Component {
       <div>
         Hello World from a User's Modal! <br />
         If all goes as planned you should see me <br />
-        when a timeslot button is clicked
+        when a timeslot button is clicked <br />
+
+        Enter in your name:
+        Enter your phone number:
       </div>
     )
   }
