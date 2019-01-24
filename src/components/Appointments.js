67643b5b31b65e4cec6c8ac2b352bb2b0904ea94
qchen3301 <q.chen.3301@gmail.com>
2019-01-24 12:14:43 -0500

    buttons firing on Appointment.js compo and storing to state

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 import * as AppActions from "../actions/AppActions"
 
 export default class Appointments extends Component {
-    // state = {
-    //     modalVisible: false,
-    //     clicks: 0,
-    //     btnColor: 'blue'
-    // }
-
-    onButtonClick = (showModal) => {
-        AppActions.showHide(showModal)
+    state = {
+        userID: 0,
+        btnColor: ""
     }
 
+    onButtonClick = (showModal, btnColor, userID) => {
+        AppActions.showHide(showModal)
+        this.setState({btnColor})
+        this.setState({userID})
+    }
 
   render() {
 
@@ -20,15 +20,15 @@ export default class Appointments extends Component {
         Hello World from Appointments Component! <br />
         Select Your Appointment Time Below <br />
 
-        <button onClick={()=> this.onButtonClick(true)}> 9AM </button>
-        <button onClick={()=>this.onButtonClick(true)}> 10 AM </button>
-        <button onClick={()=>this.onButtonClick(true)}> 11 AM </button>
-        <button onClick={()=>this.onButtonClick(true)}> 12 AM </button>
-        <button onClick={()=>this.onButtonClick(true)}> 1 PM </button>
-        <button onClick={()=>this.onButtonClick(true)}> 2 PM </button>
-        <button onClick={()=>this.onButtonClick(true)}> 3 PM </button>
-        <button onClick={()=>this.onButtonClick(true)}> 4 PM </button>
-        <button onClick={()=>this.onButtonClick(true)}> 5 PM </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 1)}> 9AM </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 2)}> 10AM </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 3)}> 11AM </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 4)}> 12 NOON </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 5)}> 1 PM </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 6)}> 2 PM </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 7)}> 3 PM </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 8)}> 4 PM </button>
+        <button onClick={()=> this.onButtonClick(true, "red", 9)}> 5 PM </button>
       </div>
     )
   }
